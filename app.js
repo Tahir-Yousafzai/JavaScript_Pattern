10237491bf6a0048c78a345ffb7fa7cdd1cee0e5
Tahir khan <tahirwpdeveloper3@gmail.com>
2024-03-24 12:37:05 +0500

    This is rectangle  shape with two step First up to down And Second down to up

---
+++ app.js
@@ -0,0 +1,16 @@
+for (var i = 1; i <= 10;  i++) {
+    for (j = 0; j < i ; j++) {
+        document.write("*" + " ")
+    }
+    document.write("<br>");
+}
+
+
+
+for(var i = 10; i>=1; i--){
+    for(var j = 1; j<i; j++){
+        document.write("*" + " ");
+    }
+    document.write("<br>")
+}
+
