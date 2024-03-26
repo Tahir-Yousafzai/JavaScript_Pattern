18ad41a30a5f3b6a9089352eea48034e4a781f0c
Tahir khan <tahirwpdeveloper3@gmail.com>
2024-03-26 08:17:21 +0500

    Four Task complete

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
-for (var i = 1; i <= 10;  i++) {
-    for (j = 0; j < i ; j++) {
-        document.write("*" + " ")
+//Pattern 1
+
+for (var i = 1; i <= 5; i++) {
+    for (var j = 1; j <= i; j++) {
+        document.write(j + " ")
     }
-    document.write("<br>");
+    document.write("<br>")
 }
 
 
+document.write("<br>");
+//Pattern 2
 
-for(var i = 10; i>=1; i--){
-    for(var j = 1; j<i; j++){
-        document.write("*" + " ");
+for (var i = 5; i >= 1; i--) {
+    for (var j = 5; j >= i; j--) {
+        document.write(j + " ")
     }
     document.write("<br>")
+}
+
+document.write("<br>")
+//Pattern 3
+
+for (var i = 1; i <= 5; i++) {
+    for (var j = 1; j <= 5; j++);{
+        document.write(j +" ")
+    }
+        document.write("<br>")
+}
+
+
+document.write("<br>")
+//Pattern 4
+
+for (var i = 1; i <= 5; i++) {
+    for (j = 0; j < i; j++) {
+        document.write("*" + " ")
+    }
+    document.write("<br>");
 }
 
