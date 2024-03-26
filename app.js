78c4bdea9a4ee508261f6631a14f6a6796bad890
Tahir khan <tahirwpdeveloper3@gmail.com>
2024-03-26 08:46:10 +0500

    Pattern 3 done

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
-//Pattern 1
+// //Pattern 1
 
-for (var i = 1; i <= 5; i++) {
-    for (var j = 1; j <= i; j++) {
-        document.write(j + " ")
-    }
-    document.write("<br>")
-}
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = 1; j <= i; j++) {
+//         document.write(j + " ")
+//     }
+//     document.write("<br>")
+// }
 
 
-document.write("<br>");
-//Pattern 2
+// document.write("<br>");
+// //Pattern 2
 
-for (var i = 5; i >= 1; i--) {
-    for (var j = 5; j >= i; j--) {
-        document.write(j + " ")
-    }
-    document.write("<br>")
-}
+// for (var i = 5; i >= 1; i--) {
+//     for (var j = 5; j >= i; j--) {
+//         document.write(j + " ")
+//     }
+//     document.write("<br>")
+// }
 
-document.write("<br>")
+// document.write("<br>")
 //Pattern 3
 
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = 1; j <= 5; j++);{
+//         document.write(j + " ")
+//     }
+//         document.write("<br>")
+// }
 for (var i = 1; i <= 5; i++) {
-    for (var j = 1; j <= 5; j++);{
-        document.write(j +" ")
+    for (j = 1; j <= 5; j++){
+        document.write(j +" ");
     }
         document.write("<br>")
 }
 
+// document.write("<br>")
+// //Pattern 4
 
-document.write("<br>")
-//Pattern 4
-
-for (var i = 1; i <= 5; i++) {
-    for (j = 0; j < i; j++) {
-        document.write("*" + " ")
-    }
-    document.write("<br>");
-}
+// for (var i = 1; i <= 5; i++) {
+//     for (j = 0; j < i; j++) {
+//         document.write("*" + " ")
+//     }
+//     document.write("<br>");
+// }
 
